403070fa511a90da90d18c91ae70530a5c7f6349
rfuzzo <r.fuzzo@gmail.com>
2021-11-18 11:20:35 +0100

    Add project files.

---
+++ RedCommunityToolkit/RedCommunityToolkit/App.xaml.cs
@@ -0,0 +1,79 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using RedCommunityToolkit.Services;
+//using Refit;
+
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace RedCommunityToolkit
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            //m_window = new MainWindow();
+            //m_window.Activate();
+
+            // Ensure the UI is initialized
+            if (Window.Current.Content is null)
+            {
+                Window.Current.Content = new Shell();
+
+                //TitleBarHelper.StyleTitleBar();
+                //TitleBarHelper.ExpandViewIntoTitleBar();
+
+                // Register services
+                Ioc.Default.ConfigureServices(
+                    new ServiceCollection()
+                    .AddSingleton<ISettingsService, SettingsService>()
+                    .BuildServiceProvider());
+            }
+
+            // Enable the prelaunch if needed, and activate the window
+            //if (args.PrelaunchActivated == false)
+            //{
+            //    CoreApplication.EnablePrelaunch(true);
+
+            //    Window.Current.Activate();
+            //}
+        }
+    }
+}
