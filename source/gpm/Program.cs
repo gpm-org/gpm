b23d32435a683f635459e2cd3fcca8fd25c39a7e
Octavian Dima <hello@wopss.net>
2021-12-11 12:30:06 +0100

    Change GenericHost to Startup and fix the logging

--- source/gpm/Program.cs
+++ source/gpm/Program.cs
@@ -1,56 +1,61 @@
-using System;
-using Serilog;
 using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Hosting;
 using System.CommandLine.Parsing;
-using System.IO;
-using gpm;
 using gpm.Commands;
-using gpm.core.Services;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
-var rootCommand = new RootCommand
+namespace gpm
 {
-    new SearchCommand(),
-    new InstallCommand(),
-    new UpdateCommand(),
-    new RemoveCommand(),
-    new ListCommand(),
-    new RestoreCommand(),
-    new UpgradeCommand(),
-    new NewCommand()
-};
-
-Log.Logger = new LoggerConfiguration()
-#if DEBUG
-    .MinimumLevel.Debug()
-#else
-    .MinimumLevel.Information()
-#endif
-    .WriteTo.Console()
-    .WriteTo.File(Path.Combine(IAppSettings.GetLogsFolder(), "gpm-log.txt"), rollingInterval: RollingInterval.Day)
-    .CreateLogger();
-
-var parser = new CommandLineBuilder(rootCommand)
-    .UseDefaults()
-    .UseHost(GenericHost.CreateHostBuilder)
-    .Build();
-
-#if DEBUG
-Environment.CurrentDirectory = GetTestSlot();
-static string GetTestSlot()
-{
-    var folder = Path.Combine(IAppSettings.GetAppDataFolder(),
-        "TESTSLOT"
-    );
-    if (!Directory.Exists(folder))
+    public static class Program
     {
-        Directory.CreateDirectory(folder);
-    }
-    return folder;
-}
-#endif
+        public static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
 
-parser.Invoke(args);
+            try
+            {
+                var rootCommand = new RootCommand
+                {
+                    new SearchCommand(),
+                    new InstallCommand(),
+                    new UpdateCommand(),
+                    new RemoveCommand(),
+                    new ListCommand(),
+                    new RestoreCommand(),
+                    new UpgradeCommand(),
+                    new NewCommand()
+                };
+
+                var parser = new CommandLineBuilder(rootCommand)
+                    .UseDefaults()
+                    .UseHost(CreateHostBuilder)
+                    .Build();
+
+                return parser.Invoke(args);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(Startup.ConfigureAppConfiguration)
+                .ConfigureServices(Startup.ConfigureServices)
+                .UseSerilog(Startup.ConfigureSerilog);
+
+    }
+}
 
 
